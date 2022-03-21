f878d4e4c1b6e2ff1f1541a5a3a212fc4c8fff08
Hemant Goswami <goswamihemant3@gmail.com>
2022-03-21 14:53:27 +0530

    21-03-2022

---
+++ stock_buy_and_sell.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#define db1(x) cerr << #x << "=" << x << '\n'
+#define db2(x, y) cerr << #x << "=" << x << "," << #y << "=" << y << '\n'
+#define db3(x, y, z) cerr << #x << "=" << x << "," << #y << "=" << y << "," << #z << "=" << z << '\n'
+#define rep(i, n) for (ll i = 0; i < (n); ++i)
+#define repA(i, a, n) for (ll i = a; i <= (n); ++i)
+#define repD(i, a, n) for (ll i = a; i >= (n); --i)
+#define mp make_pair
+#define sp << ' ' <<
+#define pb push_back
+#define FastIO               \
+    ios::sync_with_stdio(0); \
+    cin.tie(0);              \
+    cout.tie(0);
+using namespace std;
+using ll = long long;
+int main()
+{
+    FastIO
+        ll t;
+    cin >> t;
+    while (t--)
+    {
+        ll n;
+        cin >> n;
+        vector<ll> v(n);
+        rep(i, n) cin >> v[i];
+
+        cout << '\n';
+    }
+    return 0;
+}
