c534decf5d19b051996be4b1b3ad52d90df5d592
Hemant Goswami <goswamihemant3@gmail.com>
2022-03-26 15:03:44 +0530

    mod

--- sort_0_1_2.cpp
+++ sort_0_1_2.cpp
@@ -1,3 +1,8 @@
+/*Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
+
+We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+
+You must solve this problem without using the library's sort function.*/
 #include <bits/stdc++.h>
 #define db1(x) cerr << #x << "=" << x << '\n'
 #define db2(x, y) cerr << #x << "=" << x << "," << #y << "=" << y << '\n'
