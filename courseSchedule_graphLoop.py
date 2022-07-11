74e42e80cb4b7acb6102e7f60c746250d59d7916
Hemant Goswami <goswamihemant3@gmail.com>
2022-07-11 15:51:02 +0530

    11/07/22 - Graph problems starting

--- courseSchedule_graphLoop.py
+++ courseSchedule_graphLoop.py
@@ -1,31 +1,28 @@
-# striver : NEED SELF DFS TO DETECT CYCLE IN DIRECTED GRAPH
-def dfsDetectLoop(node, adj: list[list[int]], visited: list[int], dfsvisit):
-    if visited[node] == 0:
-        print("dfs", node, end=" ")
-        visited[node] = 1
-        dfsvisit[node] = 1
-    res = False
-    for i in adj[node]:
-        if dfsvisit[i] == 1:
-            return True
-        res = dfsDetectLoop(i, adj, visited, dfsvisit)
-    # when returning mark dfsvisit back to false
-    dfsvisit[node] = 0
-    return res
+# striver NEED SELF DFS TO DETECT CYCLE IN DIRECTED GRAPH
 
 
 class Solution:
     def canFinish(self, numCourses: int, prerequisites) -> bool:
         adj = [[] for i in range(numCourses)]
+        visited = [0 for i in range(numCourses)]
+        # dfsvisit = [0 for i in range(numCourses)]
         for i in prerequisites:
             adj[i[0]].append(i[1])
+
+        def dfsDetectLoop(node):
+            print(node)
+            visited[node] = 1
+            for i in adj[node]:
+
+                if visited[i] == 1:  # in this recursion visiting twice
+                    return True
+                if dfsDetectLoop(i):
+                    return True
+            visited[node] = 0
+            adj[node] = []
+            return False
+
         for i in range(numCourses):
-            # dfs with all courses as starting point , since no start course
-            visited = [0 for i in range(numCourses)]
-            dfsvisit = [0 for i in range(numCourses)]
-            if dfsDetectLoop(i, adj, visited, dfsvisit):
-                # loop is present, cant do course
+            if dfsDetectLoop(i):
                 return False
-            print()
-        # loop not present, can do course
         return True
