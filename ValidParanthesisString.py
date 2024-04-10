e4d08433f491d192ae03d61d0ea7f82c31a812fd
Hemant Goswami <goswamihemant3@gmail.com>
2024-04-10 12:06:16 +0530

    ValidParantheses

---
+++ ValidParanthesisString.py
@@ -0,0 +1,62 @@
+"""678. Valid Parenthesis String
+
+Medium
+
+Topics
+Companies
+Given a string s containing only three types of characters: '(', ')' and '*', return true if s is valid.
+
+The following rules define a valid string:
+
+Any left parenthesis '(' must have a corresponding right parenthesis ')'.
+Any right parenthesis ')' must have a corresponding left parenthesis '('.
+Left parenthesis '(' must go before the corresponding right parenthesis ')'.
+'*' could be treated as a single right parenthesis ')' or a single left parenthesis '(' or an empty string "".
+ 
+
+Example 1:
+
+Input: s = "()"
+Output: true
+Example 2:
+
+Input: s = "(*)"
+Output: true
+Example 3:
+
+Input: s = "(*))"
+Output: true
+ 
+
+Constraints:
+
+1 <= s.length <= 100
+s[i] is '(', ')' or '*'."""
+
+
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        leftMin = 0
+        leftMax = 0
+        for c in s:
+            if c == "(":
+                # adjust the ranges
+                leftMin += 1
+                leftMax += 1
+            elif c == "*":
+                leftMin -= 1
+                leftMax += 1
+            elif c == ")":
+                leftMin -= 1
+                leftMax -= 1
+            # adjust it to zero as, we want valid paranthesis
+            if leftMin < 0:
+                leftMin = 0
+            if leftMax < 0:
+                # we cannot recover from negative leftMax value, in case of "))(("
+                return False
+        print(leftMin, leftMax)
+        if leftMin <= 0 and leftMax >= 0:
+            return True
+
+        return False
