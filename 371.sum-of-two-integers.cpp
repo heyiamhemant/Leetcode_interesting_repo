eab97ede2fce521af0c96e7d77cdcee11b01b165
Hemant Goswami <goswamihemant3@gmail.com>
2025-06-26 23:58:59 +0530

    26 june 2025

---
+++ 371.sum-of-two-integers.cpp
@@ -0,0 +1,58 @@
+/*
+ * @lc app=leetcode id=371 lang=cpp
+ *
+ * [371] Sum of Two Integers
+ *
+ * https://leetcode.com/problems/sum-of-two-integers/description/
+ *
+ * algorithms
+ * Medium (52.70%)
+ * Likes:    4475
+ * Dislikes: 5719
+ * Total Accepted:    596.1K
+ * Total Submissions: 1.1M
+ * Testcase Example:  '1\n2'
+ *
+ * Given two integers a and b, return the sum of the two integers without using
+ * the operators + and -.
+ * 
+ * 
+ * Example 1:
+ * Input: a = 1, b = 2
+ * Output: 3
+ * Example 2:
+ * Input: a = 2, b = 3
+ * Output: 5
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * -1000 <= a, b <= 1000
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public:
+        int getSum(int a, int b) {
+            int carry = 0;
+            int totalSum = 0;
+            int tenPower = 1;
+            while(a || b) {
+                int sum = a % 10 + b % 10 + carry;
+                carry = 0;
+                if (sum > 10)
+                    carry = sum / 10;
+                totalSum+=((sum % 10) * tenPower) ;
+                a/=10;
+                b/=10;
+                cout << "sum : " <<sum << " carry : " << carry <<endl;
+                tenPower*=10; 
+            }
+            return totalSum + carry; 
+        }
+    };
+// @lc code=end
+
