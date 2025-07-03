ede10e9ee6d2dd9e3db66984dbb73d7049693b1e
Hemant Goswami <goswamihemant3@gmail.com>
2025-07-03 22:15:19 +0530

    3 juy 2025

--- 66.plus-one.cpp
+++ 66.plus-one.cpp
@@ -6,10 +6,24 @@
 
 // @lc code=start
 class Solution {
-public:
-    vector<int> plusOne(vector<int>& digits) {
+    public:
+        vector<int> plusOne(vector<int>& digits) {
+            int n = digits.size();
+            int sum = digits[n-1] + 1;
+            digits[n-1] = sum % 10;
+            int carry = sum / 10;
+            int pos = n-2;
+            while(carry && pos >= 0) {
+                sum = digits[pos] + 1 ;
+                digits[pos] = sum % 10;
+                carry = sum / 10;
+                pos--;
+            }
+            if(carry)
+                digits.insert(digits.begin() , 1);
         
-    }
-};
+            return digits;
+        }
+    };
 // @lc code=end
 
