d9dfc2a3d47e7a6d36d5cc473801d44ed25854ef
Hemant Goswami <goswamihemant3@gmail.com>
2025-07-27 15:27:50 +0530

    nth node from backward

---
+++ 19.remove-nth-node-from-end-of-list.cpp
@@ -0,0 +1,108 @@
+/*
+ * @lc app=leetcode id=19 lang=cpp
+ *
+ * [19] Remove Nth Node From End of List
+ *
+ * https://leetcode.com/problems/remove-nth-node-from-end-of-list/description/
+ *
+ * algorithms
+ * Medium (47.08%)
+ * Likes:    20255
+ * Dislikes: 864
+ * Total Accepted:    3.6M
+ * Total Submissions: 7.3M
+ * Testcase Example:  '[1,2,3,4,5]\n2'
+ *
+ * Given the head of a linked list, remove the n^th node from the end of the
+ * list and return its head.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: head = [1,2,3,4,5], n = 2
+ * Output: [1,2,3,5]
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: head = [1], n = 1
+ * Output: []
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: head = [1,2], n = 1
+ * Output: [1]
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * The number of nodes in the list is sz.
+ * 1 <= sz <= 30
+ * 0 <= Node.val <= 100
+ * 1 <= n <= sz
+ * 
+ * 
+ * 
+ * Follow up: Could you do this in one pass?
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+/**
+ * Definition for singly-linked list.*/
+struct ListNode {
+     int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+#include<iostream>
+using namespace std;
+
+class Solution
+{
+public:
+    ListNode *removeNthFromEnd(ListNode *head, int n)
+    {
+        int count = 0;
+        ListNode *lead = head;
+        ListNode *follow = head;
+        while (count < n)
+        {
+            lead = lead->next;
+            count++;
+        }
+        ListNode *prev = NULL;
+        while (lead)
+        {
+            prev = follow;
+            follow = follow->next;
+            lead = lead->next;
+        }
+        if (prev)
+            prev->next = follow->next;
+        else
+            // single element in list
+            head = follow->next;
+        return head;
+    }
+};
+// @lc code=end
+
