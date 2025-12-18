5f6df0fdc15a5fdd6508cc7abc67fff3896b2199
Hemant Goswami <goswamihemant3@gmail.com>
2025-12-18 10:18:46 +0530

    17 dec 2025

--- Arrayrevision.cpp
+++ Arrayrevision.cpp
@@ -1,12 +1,31 @@
 //solved https://leetcode.com/problems/score-of-a-string/
-class Solution {
-public:
-    int scoreOfString(string s) {
-        int res = 0;
-        for(int i = 1 ; i < s.size() ; i++) {
-            int score = abs( s[i] - s[i-1]);
-            res+=score;
-        }
-        return res;
+int scoreOfString(string s) {
+    int res = 0;
+    for(int i = 1 ; i < s.size() ; i++) {
+        int score = abs( s[i] - s[i-1]);
+        res+=score;
     }
-};
+    return res;
+}
+
+
+
+//https://leetcode.com/problems/longest-common-prefix/
+string longestCommonPrefix(vector<string>& strs) {
+    bool commonPrefix = true;
+    string res;
+    int i = 0;
+    while(commonPrefix && i < strs[0].size()) {
+        char common = strs[0][i];
+        for(int j = 1 ; j < strs.size() ; j++) {
+            char newCommon = strs[j][i];
+            if(common != newCommon)
+                commonPrefix = false;
+        }
+        if(commonPrefix)
+            res+=common;
+
+        i++;
+    }
+    return res;
+}
