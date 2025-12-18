49e7d9c45ffb78c7c3060d79a5da069aa5c22aaa
Hemant Goswami <goswamihemant3@gmail.com>
2025-12-18 11:15:05 +0530

    LCS 18 dec 2025

--- Arrayrevision.cpp
+++ Arrayrevision.cpp
@@ -29,3 +29,21 @@ string longestCommonPrefix(vector<string>& strs) {
     }
     return res;
 }
+
+int longestCommonSubsequence(string text1, string text2) {
+        int m = text1.size();
+        int n = text2.size();
+        int dp[m+1][n+1];
+        memset(dp, 0 , sizeof(dp));
+        for(int i = 1 ; i <= m ; i++){
+            for(int j = 1 ; j <= n ; j++) {
+                int pos1 = i-1;
+                int pos2 = j-1;
+                if(text1[pos1] == text2[pos2])
+                    dp[i][j] = 1 + dp[i-1][j-1]; // select both char
+                else 
+                    dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        return dp[m][n];
+}
