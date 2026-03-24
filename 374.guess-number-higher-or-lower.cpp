dcade2a27e8091b1827f5a8f710771618bcd559a
Hemant Goswami <goswamihemant3@gmail.com>
2026-03-25 01:49:36 +0530

    guess number higher or lower

---
+++ 374.guess-number-higher-or-lower.cpp
@@ -0,0 +1,101 @@
+/*
+ * @lc app=leetcode id=374 lang=cpp
+ *
+ * [374] Guess Number Higher or Lower
+ *
+ * https://leetcode.com/problems/guess-number-higher-or-lower/description/
+ *
+ * algorithms
+ * Easy (57.19%)
+ * Likes:    4239
+ * Dislikes: 692
+ * Total Accepted:    998.7K
+ * Total Submissions: 1.7M
+ * Testcase Example:  '10\n6'
+ *
+ * We are playing the Guess Game. The game is as follows:
+ * 
+ * I pick a number from 1 to n. You have to guess which number I picked (the
+ * number I picked stays the same throughout the game).
+ * 
+ * Every time you guess wrong, I will tell you whether the number I picked is
+ * higher or lower than your guess.
+ * 
+ * You call a pre-defined API int guess(int num), which returns three possible
+ * results:
+ * 
+ * 
+ * -1: Your guess is higher than the number I picked (i.e. num > pick).
+ * 1: Your guess is lower than the number I picked (i.e. num < pick).
+ * 0: your guess is equal to the number I picked (i.e. num == pick).
+ * 
+ * 
+ * Return the number that I picked.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: n = 10, pick = 6
+ * Output: 6
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: n = 1, pick = 1
+ * Output: 1
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: n = 2, pick = 1
+ * Output: 1
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 1 <= n <= 2^31 - 1
+ * 1 <= pick <= n
+ * 
+ * 
+ */
+
+// @lc code=start
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+#include<iostream>
+using namespace std;
+
+class Solution {
+public:
+class Solution {
+public:
+    int guessNumber(int n) {
+        int low = 1;
+        int high = n;
+        int mid = 1 + (n-1)/2;
+        while(low <= high) {
+            mid = low + (high - low)/2;
+            if(guess(mid) == 0)
+                return mid;
+            else if(guess(mid) > 0)
+                low = mid+1;
+            else if(guess(mid) < 0)
+                high = mid - 1;
+        }
+        return -1;
+    }
+};
+};
+// @lc code=end
+
