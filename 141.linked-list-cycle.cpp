3cf857b799341c13230b1fee2c4e51f8fd369658
Hemant Goswami <goswamihemant3@gmail.com>
2026-04-07 10:48:58 +0530

    Linked list cycle

--- 141.linked-list-cycle.cpp
+++ 141.linked-list-cycle.cpp
@@ -68,12 +68,13 @@
 // @lc code=start
 /**
  * Definition for singly-linked list.
+ */
 struct ListNode {
      int val;
      ListNode *next;
      ListNode(int x) : val(x), next(NULL) {}
 };
- */
+
 #include<iostream>
 using namespace std;
 class Solution {
